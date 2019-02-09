197d6ba5e51c37e650e1c453c6e21dfb687c6904
vacrim07 <vacrim07@gmail.com>
2019-02-09 17:25:56 -0500

    Actualizacion test

--- src/test/java/com/example/inadn/demo/controller/buissinesRules/impl/CheckMethodsTest.java
+++ src/test/java/com/example/inadn/demo/controller/buissinesRules/impl/CheckMethodsTest.java
@@ -2,7 +2,6 @@ package com.example.inadn.demo.controller.buissinesRules.impl;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,17 +14,12 @@ import com.example.inadn.demo.controller.buissinesRules.impl.*;
 @SpringBootTest (classes = DemoApplication.class)
 public class CheckMethodsTest {
 	
-	CheckMethods methods = null;
-	private final Integer number = 2;
-	
-	@Before
-	public void setUp() {
-		methods = new CheckMethods();
-	}
 	
 	@Test
-	public void testMethod(Integer number) {
-		assertEquals(true, methods.testMethod(number));
+	public void testMethod() {
+		Integer number = 2;
+		CheckMethods testCheckMethods = new CheckMethods();
+		assertEquals(true, testCheckMethods.testMethod(number));
 	}
 
 }
