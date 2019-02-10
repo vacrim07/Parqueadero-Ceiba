5d1677d26a977596ae0ba55b5fb903460bf9f204
vacrim07 <vacrim07@gmail.com>
2019-02-10 15:50:36 -0500

    Implementación de la lógica
    actualización

--- src/test/java/com/example/inadn/demo/model/domain/impl/ModifyParkingStatusTest.java
+++ src/test/java/com/example/inadn/demo/model/domain/impl/ModifyParkingStatusTest.java
@@ -2,6 +2,7 @@ package com.example.inadn.demo.model.domain.impl;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,10 +15,17 @@ import com.example.inadn.demo.model.impl.Parking;
 @SpringBootTest (classes = DemoApplication.class)
 public class ModifyParkingStatusTest {
 	
+	Parking p;
+	
+	@Before
+	public void setUp() {
+		p = new Parking();
+	}
+	
 	@Test
 	public void vehicleCheckOut() {
 		
-		Parking p = new Parking();
+		//Parking p = new Parking();
 		
 		p.setPosition(1);
 		ModifyParkingStatus modify = new ModifyParkingStatus(p);
