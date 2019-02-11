6aec935a155b66902a3b277747adc0da74a2b553
vacrim07 <vacrim07@gmail.com>
2019-02-10 20:16:49 -0500

    Implementación de la lógica
    Se añade caso de salida del parqueadero de una moto > 500 cc

--- src/test/java/com/example/inadn/demo/model/domain/impl/ModifyParkingStatusTest.java
+++ src/test/java/com/example/inadn/demo/model/domain/impl/ModifyParkingStatusTest.java
@@ -104,23 +104,44 @@ public class ModifyParkingStatusTest {
 		Integer position1 = 10;
 		Integer engineCapacity1 = 0;
 		GregorianCalendar checkIn1 = new GregorianCalendar(2018,11,31,20,0,0);
-		GregorianCalendar checkOut2 = new GregorianCalendar(2019,0,2,20,59,59);
+		GregorianCalendar checkOut1 = new GregorianCalendar(2019,0,2,20,59,59);
 		Motorcycle vehicle1 = new Motorcycle();
 		vehicle1.setEngineCapacity(engineCapacity1);
 		vehicle1.setType(VehicleType.CAR);
 		p1.setVehicle(vehicle1);
 		p1.setPosition(position1);
 		p1.setStartDate(checkIn1);
-		p1.setEndDate(checkOut2);
+		p1.setEndDate(checkOut1);
+		
+		// Response 2 : MOTORCYCLE > 500 CC
+		Integer position2 = 6;
+		Integer engineCapacity2 = 650;
+		GregorianCalendar checkIn2 = new GregorianCalendar(2019,1,2,20,0,0);
+		GregorianCalendar checkOut2 = new GregorianCalendar(2019,1,2,20,59,59);
+		Motorcycle vehicle2 = new Motorcycle();
+		vehicle2.setEngineCapacity(engineCapacity2);
+		vehicle2.setType(VehicleType.MOTORCYCLE);
+		p2.setVehicle(vehicle2);
+		p2.setPosition(position2);
+		p2.setStartDate(checkIn2);
+		p2.setEndDate(checkOut2);
 		
 		
 		ModifyParkingStatus response1 = new ModifyParkingStatus(p1);
+		ModifyParkingStatus response2 = new ModifyParkingStatus(p2);
 		
+		// Response 1
 		assertEquals(new Integer(48),response1.parkingHours(checkIn1, checkOut2));
 		assertEquals(false,response1.isBonusMotorcycleRequired(engineCapacity1));
 		assertEquals(new BigDecimal(16000), response1.getParking().getPrice().getAmount());
 		assertEquals(ParkingState.CHECKED_OUT.getState(),response1.getParking().getState().getState());
 		assertEquals(9,response1.getParking().getPosition().intValue());
+		// Response 2
+		assertEquals(new Integer(0),response2.parkingHours(checkIn1, checkOut2));
+		assertEquals(true,response2.isBonusMotorcycleRequired(engineCapacity1));
+		assertEquals(new BigDecimal(2000), response2.getParking().getPrice().getAmount());
+		assertEquals(ParkingState.CHECKED_OUT.getState(),response2.getParking().getState().getState());
+		assertEquals(5,response2.getParking().getPosition().intValue());
 	}
 
 }
